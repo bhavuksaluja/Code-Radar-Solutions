55bba55cb49f7b5d6d349e0d9bc83798a79cc1b7
bhavuksaluja <bhavuksaluja6@gmail.com>
2025-02-11 00:33:00 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
+
 int main() {
- int a,b,sum,multiply, subtraction;
- float division;
- char d;
- scanf("%d%d%c",&a,&b,&d);
- sum = a+b;
- multiply= a*b;
- subtraction = a-b;
- division = (float)a/b;
- if(d == '+'){
-    printf("%d",sum);
- }else if(d == '-'){
-    printf("%d",subtraction);
- }else if(d == '*') {
-    printf("%d",multiply);
- }else if(d=='/'){
-    printf("%d",division);
- } 
- else{
-    printf("error");
- }
+    int a, b, sum, multiply, subtraction;
+    float division;  // Change to float to handle decimal division
+    char d;
+
+    // Input two integers and an operator
+    scanf("%d%d%c", &a, &b, &d);
+
+    // Perform calculations
+    sum = a + b;
+    multiply = a * b;
+    subtraction = a - b;
+    division = (float)a / b;  // Explicit type casting for float division
+
+    // Output the result based on the operator
+    if (d == '+') {
+        printf("%d", sum);
+    } else if (d == '-') {
+        printf("%d", subtraction);
+    } else if (d == '*') {
+        printf("%d", multiply);
+    } else if (d == '/') {
+        printf("%.2f", division);  // Printing the division result with 2 decimal places
+    } else {
+        printf("Invalid operator");
+    }
+
     return 0;
 }
