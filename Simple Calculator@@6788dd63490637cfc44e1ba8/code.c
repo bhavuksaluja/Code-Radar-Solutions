e7d3f33731507b2071d03e0bbe7be86a314daf4c
bhavuksaluja <bhavuksaluja6@gmail.com>
2025-02-11 00:19:35 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,15 +4,15 @@ int main() {
  int a,b;
  char d;
  scanf("%d%d%c",&a,&b,&c);
- sum=a+b;
- multiply=a*b;
- subtraction =a-b;
- division = a%b;
- if(a&& b&& +){
+ int sum=a+b;
+ int multiply=a*b;
+ int subtraction =a-b;
+ int division = a%b;
+ if(d == +){
     printf("%d",sum);
- }else if(a && b &&-){
+ }else if(d == -){
     printf("%d",subtraction);
- }else if(a && b &&*) {
+ }else if(d == *) {
     printf("%d",multiply);
  }else{printf("%d",division);
  } 
