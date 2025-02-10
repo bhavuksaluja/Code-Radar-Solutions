fa41411db53b540be34fee994d933335e796aa5e
bhavuksaluja <bhavuksaluja6@gmail.com>
2025-02-11 00:28:14 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include<math.h>
 int main() {
- int a,b,sum,multiply, subtraction,division;
+ int a,b,sum,multiply, subtraction;
+ float division;
  char d;
  scanf("%d%d%c",&a,&b,&d);
  sum = a+b;
  multiply= a*b;
  subtraction = a-b;
- division = a/b;
+ division = (float)a/b;
  if(d == '+'){
     printf("%d",sum);
  }else if(d == '+'){
