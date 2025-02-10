f91eeb18c63960e06286129eef25ffed39690c4a
bhavuksaluja <bhavuksaluja6@gmail.com>
2025-02-11 00:50:33 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,29 +4,29 @@ int main() {
     int a, b, sum, multiply, subtraction;
     float division;
     char d;
-    
-    // Fix: Add spaces to allow proper reading of integers and operator
-    scanf("%d %d %c", &a, &b, &d);  // space between %d %d %c for better input handling
-
+    scanf("%d %d %c", &a, &b, &d);
     sum = a + b;
     multiply = a * b;
     subtraction = a - b;
-    division = (float)a / b;
 
-    if (d == '+') {
-        printf("%d", sum);
-    } else if (d == '-') {
-        printf("%d", subtraction);
-    } else if (d == '*') {
-        printf("%d", multiply);
-    } else if (d == '/') {
-        printf("%.0f", division);
-    }else if (d != '+','-','*','/'){
+    if (b == 0 && d == '/') {
         printf("error");
     } else {
-        printf("Invalid operators");
+        division = (float)a / b;
+
+
+        if (d == '+') {
+            printf("%d", sum);
+        } else if (d == '-') {
+            printf("%d", subtraction);
+        } else if (d == '*') {
+            printf("%d", multiply);
+        } else if (d == '/') {
+            printf("%.2f", division);
+        } else {
+            printf("Invalid operator");
+        }
     }
 
     return 0;
 }
-
