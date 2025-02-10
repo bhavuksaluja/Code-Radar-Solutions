deb67584b7106ddffe084e8d6296579bad23df53
bhavuksaluja <bhavuksaluja6@gmail.com>
2025-02-11 00:34:37 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,19 +3,13 @@
 
 int main() {
     int a, b, sum, multiply, subtraction;
-    float division;  // Change to float to handle decimal division
+    float division;
     char d;
-
-    // Input two integers and an operator
     scanf("%d%d%c", &a, &b, &d);
-
-    // Perform calculations
     sum = a + b;
     multiply = a * b;
     subtraction = a - b;
-    division = (float)a / b;  // Explicit type casting for float division
-
-    // Output the result based on the operator
+    division = (float)a / b;
     if (d == '+') {
         printf("%d", sum);
     } else if (d == '-') {
@@ -23,7 +17,7 @@ int main() {
     } else if (d == '*') {
         printf("%d", multiply);
     } else if (d == '/') {
-        printf("%.2f", division);  // Printing the division result with 2 decimal places
+        printf("%.2f", division);
     } else {
         printf("Invalid operator");
     }
